913d54e01955b619b5e71d0ee8c8af35d1ba2f33
Pablo Tesone <tesonep@gmail.com>
2026-03-23 14:33:47 +0100

    Fix pharo script

--- scripts/installBlocImage.sh
+++ scripts/installBlocImage.sh
@@ -14,7 +14,7 @@ create_vm_script() {
 	    *)          OSNAME="UNKNOWN:${unameOut}"
 	esac
 
-	VM_DIR=.
+	VM_DIR=pharo-vm
 	VM_BINARY_NAME="Pharo"
 	VM_BINARY_NAME_LINUX="pharo"
 	VM_BINARY_NAME_WINDOWS="PharoConsole"
@@ -61,7 +61,7 @@ if [ ! -f vm.zip ]; then
 	curl --progress-bar -o vm.zip https://files.pharo.org/vm/pharo-spur64-headless/$(uname -s)-$(uname -m)/stable10.zip
 fi
 
-unzip -o -d . vm.zip  
+unzip -o -d pharo-vm vm.zip  
 create_vm_script "pharo"
 
 popd
