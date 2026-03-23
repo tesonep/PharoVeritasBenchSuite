7da5b9d34d4e8af3e6312bd801298be00fdb5763
Pablo Tesone <tesonep@gmail.com>
2026-03-23 13:04:30 +0100

    Making it portable

--- scripts/installBlocImage.sh
+++ scripts/installBlocImage.sh
@@ -3,9 +3,54 @@
 set -x
 set -e
 
+create_vm_script() {
+	VM_SCRIPT=$1
+	
+	unameOut="$(uname -s)"
+	case "${unameOut}" in
+	    Linux*)     OSNAME=Linux;;
+	    Darwin*)    OSNAME=Darwin;;
+	    MSYS*|CYGWIN*|MINGW*)     OSNAME=Windows;;
+	    *)          OSNAME="UNKNOWN:${unameOut}"
+	esac
+
+	VM_DIR=.
+	VM_BINARY_NAME="Pharo"
+	VM_BINARY_NAME_LINUX="pharo"
+	VM_BINARY_NAME_WINDOWS="PharoConsole"
+
+	if [ "$OSNAME" == "Windows" ]; then
+	    PHARO_VM=`find $VM_DIR -name ${VM_BINARY_NAME_WINDOWS}.exe`
+	elif [ "$OSNAME" == "Darwin" ]; then
+	    PHARO_VM=`find $VM_DIR -name ${VM_BINARY_NAME}`
+	elif [ "$OSNAME" == "Linux" ]; then
+	    PHARO_VM=`ls $VM_DIR/${VM_BINARY_NAME_LINUX}`
+	fi
+	
+	echo "#!/usr/bin/env bash" > $VM_SCRIPT
+	echo '# some magic to find out the real location of this script dealing with symlinks
+DIR=`readlink "$0"` || DIR="$0";
+DIR=`dirname "$DIR"`;
+cd "$DIR"
+DIR=`pwd`
+cd - > /dev/null 
+# disable parameter expansion to forward all arguments unprocessed to the VM
+set -f
+# run the VM and pass along all arguments as is' >> $VM_SCRIPT
+	
+	# make sure we only substite $PHARO_VM but put "$DIR" in the script
+	echo -n \"\$DIR\"/\"$PHARO_VM\" >> $VM_SCRIPT
+		
+	# forward all arguments unprocessed using $@
+	echo " \"\$@\"" >> $VM_SCRIPT
+	
+	# make the script executable
+	chmod +x $VM_SCRIPT
+}
+
 REPO_DIR=$(readlink -f $(dirname -- "${BASH_SOURCE[0]}")/../)
 BASEDIR=$(pwd)
-VM=$BASEDIR/install_vm/Pharo.app/Contents/MacOS/Pharo
+VM=$BASEDIR/install_vm/pharo
 
 mkdir -p image
 mkdir -p install_vm
@@ -17,6 +62,8 @@ if [ ! -f vm.zip ]; then
 fi
 
 unzip -o -d . vm.zip  
+create_vm_script "pharo"
+
 popd
 
 pushd image
